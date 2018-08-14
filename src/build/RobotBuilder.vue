f1d1384fd39143b7c7bc102a336cf144106435c4
Marat <marat@deex.exchange>
2018-08-15 00:55:13 +0500

    first commit

---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'RobotBuilder',
+};
+</script>
+
+
+<style>
+
+</style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div>
+      <img  class="robot" src="../assets/robot-home.png" alt="">
+    </div>
+    <div class="get-started">
+      <a href="#">{{name}}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 300px;
+}
+</style>
